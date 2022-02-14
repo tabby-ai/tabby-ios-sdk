36aa17f468f896ec2384540c2daa16973ca5ee15
ilya.kuznetosv <kromsik@icloud.com>
2022-02-14 17:13:18 +0300

    👌 IMPROVE: TabbyCreditCardInstallmentsSnippet

--- Sources/Tabby/SwiftUI/TabbySplititPresentationalSnippet.swift
+++ Sources/Tabby/SwiftUI/TabbySplititPresentationalSnippet.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 @available(iOS 13.0, macOS 11, *)
 public struct TabbyCreditCardInstallmentsSnippet: View {
+  @Environment(\.layoutDirection) var direction
   @State private var isOpened: Bool = false
   
   func toggleOpen() -> Void {
@@ -17,106 +18,79 @@ public struct TabbyCreditCardInstallmentsSnippet: View {
   
   let amount: Double
   let currency: Currency
-  let lang: Lang
-  let url: String?
+  let withCurrencyInArabic: Bool
+  let splitPeriod: Int?
+  var urls: (String, String) = ("", "")
   
-  public init(amount: Double, currency: Currency, lang: Lang, url: String? = nil) {
+  public init(
+    amount: Double,
+    currency: Currency,
+    url: String? = nil,
+    splitPeriod: SplitPeriod? = .of4,
+    preferCurrencyInArabic: Bool? = nil
+  ) {
     self.amount = amount
     self.currency = currency
-    self.lang = lang
-    self.url = url ?? splititWebViewUrls[lang]!
+    self.withCurrencyInArabic = preferCurrencyInArabic ?? false
+    self.splitPeriod = Int(splitPeriod!.rawValue)
+    
+    if let passedUrl = url {
+      self.urls = (passedUrl, passedUrl)
+    } else {
+      let urlEn =  "\(splititWebViewUrls[.en]!)?price=\(amount)&currency=\(currency.rawValue)&splitPeriod=\(Int(splitPeriod!.rawValue))"
+      let urlAr =  "\(splititWebViewUrls[.ar]!)?price=\(amount)&currency=\(currency.rawValue)&splitPeriod=\(Int(splitPeriod!.rawValue))"
+      self.urls = (urlEn, urlAr)
+    }
   }
   
   // MARK: - BODY
   public var body: some View {
-    let isRTL = lang == Lang.ar
-    let titleEN = STRINGS_EN["presentation"]!
-    let titleAR = STRINGS_AR["presentation"]!
-    let noInterest = STRINGS_AR["noInterest"]!
-    let learnMore = !isRTL ? STRINGS_EN["learnMore"]! : STRINGS_AR["learnMore"]!
+    let isRTL = direction == .rightToLeft
+    let title = String(format: "snippetTitleCreditCard".localized, "\((amount/4).withFormattedAmount)", "\(currency.localized(l: withCurrencyInArabic && isRTL ? .ar : nil))")
+    let learnMore = String(format: "learnMore".localized)
     
     return ZStack {
       VStack(alignment: .leading) {
         HStack(alignment: .top) {
           VStack(alignment: .leading) {
-            if(!isRTL) {
-              
-              VStack(alignment: .leading, spacing: 4) {
-                Text("\(titleEN)\(SPACE)\(amount/4, specifier: "%.2f")\(SPACE)\(currency.rawValue)\(SPACE)")
-                  .foregroundColor(textPrimaryColor)
-                  .font(.footnote)
-                HStack(spacing: 4) {
-                  Text(learnMore)
-                    .foregroundColor(iris300Color)
-                    .font(.footnote)
-                    .bold()
-                    .underline()
-                  Image(systemName: "arrow.up.forward")
-                    .resizable()
-                    .foregroundColor(iris300Color)
-                    .font(Font.system(size: 10, weight: .bold))
-                    .frame(width: 10, height: 10)
-                    
-                    
-                }
-              }
-            } else {
-              VStack(alignment: .leading) {
-                HStack {
-                  Text("\(titleAR)")
-                    .foregroundColor(textPrimaryColor)
-                    .font(.footnote)
-                  Text("\(SPACE)\(amount/4, specifier: "%.2f")\(SPACE)\(currency.rawValue)")
-                    .foregroundColor(textPrimaryColor)
-                    .font(.footnote)
-                }
-                .padding(.bottom, 1)
-                HStack(alignment: .lastTextBaseline) {
-                  Text("\(noInterest)")
-                    .foregroundColor(textPrimaryColor)
-                    .font(.footnote)
-                  Text(learnMore)
-                    .foregroundColor(iris300Color)
-                    .font(.footnote)
-                    .bold()
-                    .underline()
-                }
-              }
-              .frame(minWidth: 0,
-                     maxWidth: .infinity,
-                     minHeight: 0,
-                     alignment: .leading)
+            VStack(alignment: .leading, spacing: 4) {
+              Text(title)
+                .foregroundColor(textPrimaryColor)
+                .font(.system(size: 12))
+              + Text(learnMore)
+                .foregroundColor(iris300Color)
+                .font(.system(size: 12, weight: .bold))
+                .underline()
             }
+            .frame(minWidth: 0,
+                   maxWidth: .infinity,
+                   minHeight: 0,
+                   alignment: .leading)
           }
-          .frame(minWidth: 0,
-                 maxWidth: .infinity,
-                 minHeight: 0,
-                 alignment: .leading)
           Logo()
         }
+        .padding(.horizontal, 16)
+        .padding(.vertical, 16)
+        .background(Color(.white))
+        .cornerRadius(8)
+        .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.25), radius: 3, x: 0, y: 2)
+        .padding(.horizontal, 16)
+        .environment(\.layoutDirection, isRTL ? .rightToLeft : .leftToRight)
+        .onTapGesture {
+          toggleOpen()
+        }
       }
-      .padding(.horizontal, 16)
-      .padding(.vertical, 16)
-      .background(Color(.white))
-      .cornerRadius(8)
-      .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.25), radius: 3, x: 0, y: 2)
-      .padding(.horizontal, 16)
-      .environment(\.layoutDirection, isRTL ? .rightToLeft : .leftToRight)
-      .onTapGesture {
-        toggleOpen()
-      }
+      .sheet(isPresented: $isOpened, content: {
+        SafariView(lang: isRTL ? Lang.ar : Lang.en, customUrl: isRTL ? self.urls.1 : self.urls.0)
+      })
     }
-    .sheet(isPresented: $isOpened, content: {
-      SafariView(lang: isRTL ? Lang.ar : Lang.en, customUrl: self.url)
-    })
   }
 }
 
 struct TabbyCreditCardInstallmentsSnippetPreview: PreviewProvider {
   static var previews: some View {
     VStack{
-      TabbyCreditCardInstallmentsSnippet(amount: 1990, currency: .SAR, lang: Lang.en)
-      TabbyCreditCardInstallmentsSnippet(amount: 1990, currency: .SAR, lang: Lang.ar)
+      TabbyCreditCardInstallmentsSnippet(amount: 1990, currency: .SAR)
     }
   }
 }
