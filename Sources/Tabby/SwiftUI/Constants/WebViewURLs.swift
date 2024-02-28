cfc52135ae326f89abc747184572fc6083ba5c75
Grigorii Berngardt <grigorii.berngardt@tabby.ai>
2024-02-28 18:59:11 +0400

    [TA-21633] Analytics events integration and refactorings

--- Sources/Tabby/SwiftUI/Constants/WebViewURLs.swift
+++ Sources/Tabby/SwiftUI/Constants/WebViewURLs.swift
@@ -1,18 +1,14 @@
-//
-//  WebViewURLs.swift
-//  
-//
-//  Created by ilya.kuznetsov on 30.08.2021.
-//
-
 import Foundation
 
-let webViewUrls: [Lang:String] = [
-    Lang.en: "https://checkout.tabby.ai/promos/product-page/installments/en/",
-    Lang.ar: "https://checkout.tabby.ai/promos/product-page/installments/ar/",
-]
-
-let splititWebViewUrls: [Lang:String] = [
-    Lang.en: "https://checkout.tabby.ai/promos/checkout-page/credit-card-installments/en/",
-    Lang.ar: "https://checkout.tabby.ai/promos/checkout-page/credit-card-installments/ar/",
-]
+enum WebViewBaseURL {
+    
+    enum Tabby {
+        static let en = "https://checkout.tabby.ai/promos/product-page/installments/en/"
+        static let ar = "https://checkout.tabby.ai/promos/product-page/installments/ar/"
+    }
+    
+    enum Splitit {
+        static let en = "https://checkout.tabby.ai/promos/checkout-page/credit-card-installments/en/"
+        static let ar = "https://checkout.tabby.ai/promos/checkout-page/credit-card-installments/ar/"
+    }
+}
